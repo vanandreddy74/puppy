3700bf0c881f9e67b4387486bf1ea823958e0520
vanandhpe <var@hpe.com>
2023-06-26 23:38:13 +0530

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vanandreddy74/puppy
 
 go 1.17
+
+require github.com/vanandreddy74/dog v0.0.0-20230626180027-4d368c02e2ef // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/vanandreddy74/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownup(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownup(Barks())
 }
